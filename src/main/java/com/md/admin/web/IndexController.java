939ee68f3856e1fc25d6e247dc24d9611e224787
tangyue <tangyue@gosotech.com>
2019-12-19 14:31:29 +0800

    first commit

---
+++ src/main/java/com/md/admin/web/IndexController.java
@@ -0,0 +1,17 @@
+/**
+ * chenxitech.cn Inc. Copyright (c) 2017-2019 All Rights Reserved.
+ */
+package com.md.admin.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author tangyue
+ * @version $Id: IndexController.java, v 0.1 2019-12-17 16:10 tangyue Exp $$
+ */
+@RestController
+public class IndexController {
+
+
+}
