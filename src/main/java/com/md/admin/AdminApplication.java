e0c495f1c3d6360d2fb0dd8ee09cde519d7c7211
tangyue <tangyue@gosotech.com>
2020-01-02 17:25:55 +0800

    新增谷歌翻译api接口

--- src/main/java/com/md/admin/AdminApplication.java
+++ src/main/java/com/md/admin/AdminApplication.java
@@ -2,6 +2,7 @@ package com.md.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -9,7 +10,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class AdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminApplication.class, args);
+
+        SpringApplication springApplication = new SpringApplication(AdminApplication.class);
+        //支持生成 pid 文件
+        springApplication.addListeners(new ApplicationPidFileWriter("admin.pid"));
+        springApplication.run(AdminApplication.class, args);
     }
 
 }
